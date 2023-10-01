992692e6211ae273350a6b962085b215a54ef07f
ccharfu <carlosuefagh@gmail.com>
2023-10-01 07:15:13 +0000

    cambios en todos los puntos finales

--- FuncionesEstado.py
+++ FuncionesEstado.py
@@ -1,7 +1,18 @@
 import pandas as pd
 
-def EstadoCanalF(Array,Posiciones,tam):
-    # print(type(Posiciones))
+def count_estados(Posiciones):
+    diccionario_Posiciones = {}
+    for sublista in Posiciones:
+        llave = sublista[0]
+        leng_sub = len(sublista) - 1
+        diccionario_Posiciones[llave] = leng_sub
+    return diccionario_Posiciones
+
+def EstadoCanalF(Array,Posiciones):
+    count_positions = count_estados(Posiciones)
+    print("\n")
+    print(count_positions)
+    print("\n")
     my_dic = {}
     lista1 = [[] for _ in range(len(Posiciones))]
     lista2 = [[] for _ in range(len(Posiciones))]
@@ -16,119 +27,101 @@ def EstadoCanalF(Array,Posiciones,tam):
             lista1[x].append(Array[0][Posiciones[x][y+1]])
             lista2[x].append(Array[1][Posiciones[x][y+1]])
             lista3[x].append(Array[2][Posiciones[x][y+1]])
-            y +=1
-    # print(lista1)
-    # print(lista2)
-    # print(lista3)
+            y += 1
     
     my_dic["A"] = lista1
     my_dic["B"] = lista2
     my_dic["C"] = lista3
-    print(my_dic)
+
+    nuevo_diccionario = {}
+    for clave_principal, lista_secundaria in my_dic.items():
+        nueva_lista = {}
+        for elemento in lista_secundaria:
+            clave_secundaria = elemento[0]
+            nueva_lista[clave_secundaria] = elemento[1:]
+        nuevo_diccionario[clave_principal] = nueva_lista
+    
+    print("\n")
+    print(nuevo_diccionario)
     print("\n")
 
-    proporcion_dict = {}
+    resultados = {}
+    for clave_principal, subdiccionario in nuevo_diccionario.items():
+        sub_resultados = {}
+        for subllave, lista in subdiccionario.items():
+            cantidad_de_unos = lista.count(1)
+            sub_resultados[subllave] = cantidad_de_unos
+        # print(sub_resultados)
+        # print("\n")
+        division_resultados = {}
+        for llave in sub_resultados:
+            if llave in count_positions:
+                print(f'{llave} === {sub_resultados[llave]} / { count_positions[llave]}')
+                division_resultados[llave] = sub_resultados[llave] / count_positions[llave]
+        print("\n")
+        resultados[clave_principal] = division_resultados
 
-    for key, value in my_dic.items():
-        proporciones = []
-        for sublist in value:
-            total_elementos = len(sublist) - 1  # Restamos 1 para no contar el primer elemento
-            total_unos = sum(sublist[1:])  # Sumamos todos los unos en la sublista
-            proporcion = total_unos / total_elementos if total_elementos > 0 else 0  # Evitamos la división por cero
-            proporciones.append([sublist[0], proporcion])
-        proporcion_dict[key] = proporciones
+    for data, val in resultados.items():
+        print(f'{data}:{val}')
 
-    print(proporcion_dict)
     print("\n")
+    print(count_positions)
+    print("\n")
+
+    # # Crear un DataFrame a partir del diccionario
+    # df = pd.DataFrame(resultados)
+    # print(df)
+# --------------------------------------------------------------------------------
+
+# Punto 2
+def EstadoEstadoF(Array,Posiciones,Estados):
+    lista = []
+    my_dic = {}
+    for x in range(len(Posiciones)):        
+        y=0
+        while y < len(Posiciones[x])-1 and Posiciones[x][y+1] != 30:
+            data = str(Array[0][Posiciones[x][y+1]])+ str(Array[1][Posiciones[x][y+1]])+ str(Array[2][Posiciones[x][y+1]])
+            lista.append(data)
+            y+=1;
+        my_dic[Posiciones[x][0]] = lista
+        lista = []
+    
+    # Diccionario resultante
+    diccionario_r = {}
+
+    # Recorremos el diccionario original
+    for estado, sublistas in my_dic.items():
+        temp_count = {} # Diccioanrio temporal
+        for sublista in sublistas:
+            if sublista in temp_count:
+                temp_count[sublista] += 1
+            else:
+                temp_count[sublista] = 1
+        for est in Estados:
+            if est not in temp_count:
+                temp_count[est] = 0
+        diccionario_r[estado] = temp_count
+   
+    diccionario_Posiciones = count_estados(Posiciones)
+   
+    result_dict = {}
+
+    for key2, value2 in diccionario_Posiciones.items():
+        if key2 in diccionario_r:
+            result_dict[key2] = {}
+            for key1, value1 in diccionario_r[key2].items():
+                result_dict[key2][key1] = value1 / value2
 
     # Crear un DataFrame a partir del diccionario
-    df = pd.DataFrame({key: [val[1] for val in value] for key, value in proporcion_dict.items()}, index=[val[0] for val in next(iter(proporcion_dict.values()))])
-
-    # Renombrar las columnas con los nombres de las claves
-    df.columns = proporcion_dict.keys()
-
+    df = pd.DataFrame(result_dict)
+    df = df.sort_index(axis=0).sort_index(axis=1).transpose()
     print(df)
 
 # --------------------------------------------------------------------------------
-
-
-
-def EstadoEstadoF(Array,Posiciones,Estados):
-    Estados=[["Estado",]]
-    for x in range(len(Posiciones)):
-        print(Posiciones[x][0])
-        Estados[0].append(Posiciones[x][0])
-        Estados.append([Posiciones[x][0]])
-        #for x in range(len(Posiciones[x])):
-        #   print(Estados.index(Posiciones[x][0]))
-
-    print(Estados[0])
-    print(Estados[1])
-    print(Estados[2])
-    print(Estados[3])
-    print(Estados[4])
-    print(Estados[5])
-    print(Estados[6])
-    print(Estados[7])
-
-    for x in range(len(Posiciones)):
-        print("Siquientes Elementos de "+Posiciones[x][0])
-        y=0
-        while y<len(Posiciones[x])-1 and  Posiciones[x][y+1] != 30:
-            print("elemento de siquiente posicion :")
-            print(str(Array[0][Posiciones[x][y+1]])+
-                  str(Array[1][Posiciones[x][y+1]])+
-                  str(Array[2][Posiciones[x][y+1]]))
-            y+=1;
-# --------------------------------------------------------------------------------
-
 # Punto 3
 def EstadoCanalP(Array,Posiciones):
-    for x in range(len(Posiciones)):
-        print(Posiciones[x][0]+" Sus siquientes Posiciones son: ")
-        y = 0
-        while y<len(Posiciones[x])-1 and Posiciones[x][y+1] != 30 :
-            print("siquiente elemento por posicion posicion:")
-            print(Array[0][Posiciones[x][y+1]])
-            print(Array[1][Posiciones[x][y+1]])
-            print(Array[2][Posiciones[x][y+1]])
-            y +=1
+    EstadoCanalF(Array,Posiciones)
 # --------------------------------------------------------------------------------
-
 # Punto 4
 def EstadoEstadoP(Array,Posiciones,Estados):
-    Estados=[["Estado",]]
-    for x in range(len(Posiciones)):
-        print(Posiciones[x][0])
-        Estados[0].append(Posiciones[x][0])
-        Estados.append([Posiciones[x][0]])
-        #for x in range(len(Posiciones[x])):
-        #   print(Estados.index(Posiciones[x][0]))
-
-    print(Estados[0])
-    print(Estados[1])
-    print(Estados[2])
-    print(Estados[3])
-    print(Estados[4])
-    print(Estados[5])
-    print(Estados[6])
-    print(Estados[7])
-
-    nuevos = []
-
-    print("---------")
-    for x in range(len(Posiciones)):
-        print("Siquientes Elementos de "+Posiciones[x][0])
-        y=0
-        while y<len(Posiciones[x])-1 and  Posiciones[x][y+1] != 30:
-            print(str(Array[0][Posiciones[x][y+1]])+
-                  str(Array[1][Posiciones[x][y+1]])+
-                  str(Array[2][Posiciones[x][y+1]]))
-            nuevos.append(str(Array[0][Posiciones[x][y+1]])+
-                  str(Array[1][Posiciones[x][y+1]])+
-                  str(Array[2][Posiciones[x][y+1]]))
-            y+=1;
-        print("\n");
-    print(nuevos)
-
-
+    EstadoEstadoF(Array,Posiciones,Estados)
--- Main.py
+++ Main.py
@@ -1,74 +1,92 @@
-import FuncionesEstado as F;
+import FuncionesEstado as F
 
+Entrada1 = [0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1]
+Entrada2 = [0, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0]
+Entrada3 = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1,0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
 
-Entrada1 = [0,1,1,0,1,1,0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,0,0,0,1];
-Entrada2 = [0,0,0,1,1,0,1,0,1,0,1,0,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,0,0,0];
-Entrada3 = [0,1,0,1,0,1,0,1,0,1,1,0,1,1,0,1,1,1,1,0,1,0,1,0,1,0,1,0,1,0];
-
-def EncontrarPosiciones(Estados,Array):
-    Posiciones=[]
+def EncontrarPosiciones(Estados, Array):
+    Posiciones = []
     for i in range(len(Estados)):
-        Posiciones.append([Estados[i]]);
+        Posiciones.append([Estados[i]])
     for m in range(len(Estados)):
-        Valor = Estados[m];
+        Valor = Estados[m]
         for x in range(len(Array[0])):
-            if((str(Array[0][x])+str(Array[1][x])+str(Array[2][x])) == str(Valor)):
-                Posiciones[m].append(x+1);
-    #print(Posiciones);
-    return Posiciones;
-    
+            if ((str(Array[0][x])+str(Array[1][x])+str(Array[2][x])) == str(Valor)):
+                Posiciones[m].append(x+1)
+    return Posiciones
+
+
 def EncontrarPosicionesR(Posiciones):
     arregloR = []
 
     for sublista in Posiciones:
         nueva_sublista = [sublista[0]]
-
         for valor in sublista[1:]:
             if valor > 1:
                 nuevo_valor = valor - 2
                 nueva_sublista.append(nuevo_valor)
-
         arregloR.append(nueva_sublista)
-
     return arregloR
 
-def CrearEstados(Array):
-    Estados =[];
-    Estadoinicial="";
-    Estadoinicial = Estadoinicial.zfill(len(Array));
-    Estados.append(Estadoinicial)
-    siquiente = Estadoinicial;
-    for i in range((2**len(Array))-1):
-        siquiente = '{:b}'.format(int(siquiente,2)+int(1))
-        Estados.append(siquiente.zfill(len(Array)))
+
+def mostrar_menu(Array,Posiciones,Estados,PosicionesR):
+    print("Menu:")
+    print("1. Estado Canal F")
+    print("2. Estado Estado F")
+    print("3. Estado Canal P")
+    print("4. Estado Estado P")
+    print("5. Salir")
    
-    Posiciones= EncontrarPosiciones(Estados,Array);  
-    PosicionesR= EncontrarPosicionesR(Posiciones);
-
+    opcion = input("Selecciona una opción (1/2/3/4/5): ")
     
-    # print('-------')
+    if opcion == "1":
+        # Punto 1: Llama a la función correspondiente
+        F.EstadoCanalF(Array, Posiciones)
+    elif opcion == "2":
+        # Punto 2: Llama a la función correspondiente
+        F.EstadoEstadoF(Array, Posiciones, Estados)
+    elif opcion == "3":
+        # Punto 3: Llama a la función correspondiente
+        F.EstadoCanalP(Array, PosicionesR)
+    elif opcion == "4":
+        # Punto 4: Llama a la función correspondiente
+        F.EstadoEstadoP(Array, PosicionesR, Estados)
+    else:
+        print("Opción no válida. Por favor, selecciona una opción válida.")
+
+
+def CrearEstados(Array):
+    Estados = []
+    Estadoinicial = ""
+    Estadoinicial = Estadoinicial.zfill(len(Array))
+    Estados.append(Estadoinicial)
+    siquiente = Estadoinicial
+    for i in range((2**len(Array))-1):
+        siquiente = '{:b}'.format(int(siquiente, 2)+int(1))
+        Estados.append(siquiente.zfill(len(Array)))
+
+    Posiciones = EncontrarPosiciones(Estados, Array)
+    PosicionesR = EncontrarPosicionesR(Posiciones)
+
+    # # Punto 1
+    # F.EstadoCanalF(Array,Posiciones)
+
+    # # Punto 2
+    # F.EstadoEstadoF(Array,Posiciones,Estados)
+
+    # # Punto 3
+    # print("\n")
     # print(Posiciones)
-    # print('-------')
-    #print('*********')
-    #print(PosicionesR)
-    #print('-------')
-    
-    # Punto 1
-    F.EstadoCanalF(Array,Posiciones,len(Array))
-
-    # Punto 2
-    #F.EstadoEstadoF(Array,Posiciones,Estados)
-
-    # Punto 3
-    #F.EstadoCanalP(Array,PosicionesR)
-    
-    # print(Array)
-    # print("---------")
+    # print("\n")
     # print(PosicionesR)
-    # print("---------")
-    # print(Estados)
+    print(Estados)
+    # print("\n")
+    # F.EstadoCanalP(Array, PosicionesR)
 
-    # Punto 4
+    # # Punto 4
     # F.EstadoEstadoP(Array,PosicionesR,Estados)
-    
-CrearEstados([Entrada1,Entrada2,Entrada3]);
+    # mostrar_menu(Array,Posiciones,Estados,PosicionesR)
+
+
+CrearEstados([Entrada1, Entrada2, Entrada3])
+
